5f418937e6bfbaf85d90873889bbcd640376497d
Eric Ghoubiguian <ericghoubiguian@live.fr>
2020-11-15 16:40:45 +0100

    Define a CurrentWeather class

--- hello_world.scala
+++ hello_world.scala
@@ -1,7 +1,13 @@
+class CurrentWeather(var city:String, var apiKey:String, var countryCode:String = "") {
+
+	
+}
+
 object hello_world {
 
 	def main(args: Array[String]) = {
-
+		
+		var test = new CurrentWeather("JJ", "KK", "OO")
 		println("Hello, new language named Scala !")
 	}
 }
